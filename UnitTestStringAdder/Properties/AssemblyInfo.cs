869bb05fad5b043ff8d98a35bc303240d58a9e00
serp777 <arusse02@calpoly.edu>
2018-10-08 22:48:58 -0700

    AJR: Initial string adder C# console and unit test project

---
+++ UnitTestStringAdder/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestStringAdder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestStringAdder")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("144ab5bc-1266-4a69-9072-c41e8aebf117")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestStringAdder/StringAdderTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestStringAdder
+{
+    [TestClass]
+    public class StringAdderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
